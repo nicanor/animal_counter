286fee96825a8710c68a6f6b27c2a9705a98caeb
Nicanor Perera <nicanorperera@gmail.com>
2025-06-11 09:16:33 -0300

    Address user formatting preferences

--- animal_counter.js
+++ animal_counter.js
@@ -1,9 +1,10 @@
-  var quantities =  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
-  var velocities =  [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+"use strict";
 
-  function StartCounter() {
+const quantities = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+const velocities = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
 
-    var units = [ 
+function startCounter() {
+  const units = [
     49877536490, // gallinas
      2676365000, // patos
      1375940758, // cerdos
@@ -14,31 +15,34 @@
        23199336, // bufalos
         5018470, // caballos
         1501799  // camellos y otros camelidos
-    ];
+  ];
 
-    var bySecond = 4;
-    for (var i = 0; i < quantities.length; ++i){ 
-      velocities[i] = units[i] / 365 / 24 / 60 / 60 / bySecond;
-    }
-    setInterval("UpdateCounter()", 1000 / bySecond);
+  const updatesPerSecond = 4;
+  for (let i = 0; i < quantities.length; i++) {
+    velocities[i] = units[i] / 365 / 24 / 60 / 60 / updatesPerSecond;
   }
+  setInterval(updateCounter, 1000 / updatesPerSecond);
+}
 
-  function UpdateCounter() {
-    var num, thous, str;
-    for (var i = 0; i < quantities.length; ++i) {
-      quantities[i] += velocities[i];
-      num = Math.round(quantities[i]);
-      str = "";
-      while (num > 1000) {
-        thous = num % 1000;
-        if (thous < 10)
-          thous = "00" + thous;
-        else if (thous < 100)
-          thous = "0" + thous;
-        str = "," + thous + str;
-        num = Math.floor(num / 1000);
+function updateCounter() {
+  for (let i = 0; i < quantities.length; i++) {
+    quantities[i] += velocities[i];
+    let num = Math.round(quantities[i]);
+    let str = "";
+    while (num > 1000) {
+      let thous = num % 1000;
+      if (thous < 10) {
+        thous = "00" + thous;
+      } else if (thous < 100) {
+        thous = "0" + thous;
       }
-      str = num + str;
-      document.getElementById("cant" + i).innerHTML = str;
+      str = "," + thous + str;
+      num = Math.floor(num / 1000);
+    }
+    str = num + str;
+    const target = document.getElementById("cant" + i);
+    if (target) {
+      target.textContent = str;
     }
   }
+}
